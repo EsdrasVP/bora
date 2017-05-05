311cd632bd8b9689645a6f883f92f0f2361452d2
EsdrasVP <esdras015@gmail.com>
2017-05-05 10:50:50 -0300

    Commiting maven project structure

---
+++ src/main/java/core/Attendee.java
@@ -0,0 +1,12 @@
+package core;
+
+public class Attendee {
+	
+	private String email;
+	private String name;
+
+	public Attendee(String email, String name) {
+		this.email = email;
+		this.name = name;
+	}
+}
